f0e573b85f49bd71b2eab3276e81e85e18eed3a0
evgeniiijjj <evgenijjj78@gmail.com>
2022-09-02 09:39:01 +0300

    first commit

---
+++ src/main/java/org/example/controller/PostController.java
@@ -0,0 +1,59 @@
+package org.example.controller;
+
+import com.google.gson.Gson;
+import org.example.exception.NotFoundException;
+import org.example.model.Post;
+import org.example.service.PostService;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.Reader;
+
+public class PostController {
+  public static final String APPLICATION_JSON = "application/json";
+  private final PostService service;
+
+  public PostController(PostService service) {
+    this.service = service;
+  }
+
+  public void all(HttpServletResponse response) throws IOException {
+    response.setContentType(APPLICATION_JSON);
+    final var data = service.all();
+    final var gson = new Gson();
+    response.getWriter().print(gson.toJson(data));
+  }
+
+  public void getById(long id, HttpServletResponse response) throws IOException {
+    // TODO: deserialize request & serialize response
+    response.setContentType(APPLICATION_JSON);
+    try {
+      final var data = service.getById(id);
+      final var gson = new Gson();
+      response.getWriter().print(gson.toJson(data));
+    } catch (NotFoundException e) {
+      response.sendError(HttpServletResponse.SC_NOT_FOUND);
+    }
+  }
+
+  public void save(Reader body, HttpServletResponse response) throws IOException {
+    response.setContentType(APPLICATION_JSON);
+    final var gson = new Gson();
+    final var post = gson.fromJson(body, Post.class);
+    try {
+      final var data = service.save(post);
+      response.getWriter().print(gson.toJson(data));
+    } catch (NotFoundException e) {
+      response.sendError(HttpServletResponse.SC_NOT_FOUND);
+    }
+  }
+
+  public void removeById(long id, HttpServletResponse response) throws IOException {
+    // TODO: deserialize request & serialize response
+    try {
+      service.removeById(id);
+    } catch (NotFoundException e) {
+      response.sendError(HttpServletResponse.SC_NOT_FOUND);
+    }
+  }
+}
