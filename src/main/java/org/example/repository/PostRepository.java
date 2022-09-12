b46278eb8362655309b9576d68b29bc73eb12835
evgeniiijjj <100117060+evgeniiijjj@users.noreply.github.com>
2022-09-12 09:45:56 +0300

    Update PostRepository.java

--- src/main/java/org/example/repository/PostRepository.java
+++ src/main/java/org/example/repository/PostRepository.java
@@ -7,10 +7,10 @@ import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 
-// Stub
+
 public class PostRepository {
 
-  private long lastId;
+  private volatile long lastId;
   private final Map<Long, Post> posts;
 
   public PostRepository() {
